ea47dc3a2c45a282345a4a5f152bf21b618cc496
Valentin Himself <valentinhimself@gmail.com>
2022-02-23 18:39:07 -0700

    testing things

--- script/app.js
+++ script/app.js
@@ -11,23 +11,27 @@ function operate (operator, num1, num2) {
         case "*": 
             return multiply(num1, num2);
         case "/": 
-            return divide(num1, num2);
-         
+            return divide(num1, num2);      
     }
-   
+
     function add(num1, num2) {
         return num1 + num2;
     }
-    
+
     function subtract(num1, num2) {
         return num1 - num2;
     }
-    
+
     function multiply(num1, num2) {
         return num1 * num2;
     }
-    
+
     function divide(num1, num2) {
         return num1/num2;
     }
 }
+
+const numbers = document.querySelectorAll ('button');
+numbers.forEach(number => number.addEventListener('click', function () {
+    console.log(this.value);
+}));
