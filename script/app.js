a4da9dd8e00038f8f29faa36a1b081a53b5ece62
Valentin Himself <valentinhimself@gmail.com>
2022-02-25 09:51:59 -0700

    synch

--- script/app.js
+++ script/app.js
@@ -48,7 +48,6 @@ function passValue(input) {
     function addNumber (input) {
         let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
         transformDecimal(input);
-        
         if (currentNumber.includes('.') && input === '.') {
             //do nothing
         }
